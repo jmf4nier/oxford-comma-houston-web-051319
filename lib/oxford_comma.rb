d9ad76b10290886a73fe4dbd4a502221e46dacb6
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 11:22:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,15 @@
-array = ["jason", "micah", "souki", "aaron"]
-
+#array = ["jason", "micah", "souki", "aaron"]
+array = ["jason", "souki"]
+#array = ["jason"]
 def oxford_comma(array)
   if array.length > 1
     new_array = "and " + array.pop
     array.push(new_array)
     array.join(", ")
+  elsif array.length == 2 
+    array.insert[1, "and"]
   end
-  array.join
+  
 end
 
 #oxford_comma(array)
