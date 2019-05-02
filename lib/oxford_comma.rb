56602d3fb821945c2f1d074dd1c62eba490b9539
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 11:05:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ array = ["jason", "micah", "souki", "aaron"]
 
 def oxford_comma(array)
    new_array = array.insert(-2, "and")
-   new_array[-1, -2].join
-  
+   new_array[-1, -2]
 end
 
 oxford_comma(array)
