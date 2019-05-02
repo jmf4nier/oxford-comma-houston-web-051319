1d58f376dab707fa658336a667a6dd098fed60fe
Jason Fournier <jmf4nier@hotmail.com>
2019-05-02 11:17:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 array = ["jason", "micah", "souki", "aaron"]
 
 def oxford_comma(array)
-  if array.size > 1
+  if array.length > 1
     new_array = "and " + array.pop
     array.push(new_array)
     array.join(", ")
